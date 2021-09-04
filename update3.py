2f1bcdb6207c9a6fe92a703a6029f51e3f2d0a9d
Jean-Jacques François Reibel <9219877+JJFReibel@users.noreply.github.com>
2021-09-04 11:20:20 -0700

    added update3.py

---
+++ update3.py
@@ -0,0 +1,22 @@
+# Double Headed Snake
+# By Jean-Jacques F. Reibel
+# I will not be held responsible for:
+# any shenanigans
+import os
+
+
+os.system("printf '\e[1;32;1;1m            ～～～～～～～～～～～～('")
+os.system("printf '\e[0;33;1;1m°'")
+os.system("printf '\e[0;31;1;1m<'")
+os.system("printf '\e[0;31;1;5m~\n'")
+os.system("printf '\e[0;32;1;1m          /\n'")
+os.system("printf '\e[0;32;1;1m         |\n'")
+os.system("printf '\e[0;32;1;1m          \\'")
+os.system("printf '\e[1;32;1;1m\n            ～～～～～～～～～～～～～\n'")
+os.system("printf '\e[0;32;1;1m                                       \\\n'")
+os.system("printf '\e[0;32;1;1m                                        |\n'")
+os.system("printf '\e[0;32;1;1m                                       /\\n'")
+os.system("printf '\e[0;31;1;5m          ~'")
+os.system("printf '\e[0;31;1;1m>'")
+os.system("printf '\e[0;33;1;1m°'")
+os.system("printf '\e[1;32;1;1m)～～～～～～～～～～～～  \n\n'")
